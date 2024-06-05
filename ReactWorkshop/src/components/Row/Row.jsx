2d0b9e6ea40a56fbf20544748d1985f8ec9f53f2
faseian <nab4nkert@outlook.com>
2024-06-05 00:59:59 -0700

    Never forget to import the css dumbass

--- ReactWorkshop/src/components/Row/Row.jsx
+++ ReactWorkshop/src/components/Row/Row.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-
 import Block from "./Block";
+import "./Row.css"
 
 function Row() {
     return (
-        <div className="row-container">
-            <Block></Block>
-            <Block></Block>
-            <Block></Block>
-            <Block></Block>
+        <div className="section-container">
+            <h3>Genre Ig</h3>
+            <div className="row-container">
+                <Block></Block>
+                <Block></Block>
+                <Block></Block>
+                <Block></Block>
+                <Block></Block>
+                <Block></Block>
+            </div>
         </div>
     )
 }
