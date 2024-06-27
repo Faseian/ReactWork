ce2c420528c35459a4c7ffce38afdffc2f9cd700
faseian <nab4nkert@outlook.com>
2024-06-27 15:16:35 -0700

    start redux store to get data from api

--- ReactWorkshop/src/components/Row/Block.jsx
+++ ReactWorkshop/src/components/Row/Block.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Block.css"
 
-function Block() {
+function Block(props) {
     function someFunction() {
         console.log("tess is a fart monkey");
     }
 
     return (
-        <div className = "block-container">
+        <div className = "block-container" key={props.myanimelist_id}>
             <img src="https://picsum.photos/200/300" alt="" className="block-img"/>
-            <h1 className="block-title">Title</h1>
-            <p className="block-description">description</p>
+            <h1 className="block-title">{props.title}</h1>
+            <p className="block-score">Score: {props.score} Rank: {props.rank}</p>
             <button onClick={someFunction} className="block-btn">More Info</button>
         </div>
     )
--- ReactWorkshop/src/components/Row/Row.jsx
+++ ReactWorkshop/src/components/Row/Row.jsx
@@ -3,25 +3,35 @@ import Block from "./Block";
 import "./Row.css"
 
 function Row() {
-    /*
-    async function logAnimes() {
-        const url = 'https://api.themoviedb.org/3/account/null/lists?page=1';
+    /*async function logAnimes() {
+        const url = 'https://myanimelist.p.rapidapi.com/anime/top/all';
         const options = {
             method: 'GET',
+            param: {
+                'n': 10
+            },
             headers: {
-                accept: 'application/json',
-                Authorization: ''
+                'x-rapidapi-key': '80e8cae6e6mshb20aa21a8f7babbp1602c6jsna179ded48bab',
+                'x-rapidapi-host': 'myanimelist.p.rapidapi.com'
             }
         };
 
             fetch(url, options)
                 .then(res => res.json())
-                .then(json => console.log(json))
+                .then(json => 
+                    {
+                        json.forEach(element => {
+                            const block = React.createElement("Block", json[element]);
+                        });
+                    })
                 .catch(err => console.error('error:' + err));
     }
 
     logAnimes();
     */
+
+    
+
     return (
         <div className="section-container">
             <h3 className="genre-container">Genre Ig</h3>
