6d55e8675bf9aece40134985e799503d9e19318b
faseian <nab4nkert@outlook.com>
2024-07-18 14:06:50 -0700

    Cleanup

--- ReactWorkshop/src/App.jsx
+++ ReactWorkshop/src/App.jsx
@@ -18,7 +18,6 @@ function App() {
       <Header/>
       {fetchedGenres ?
       fetchedGenres.map(genres => {
-
         if(genres._id === 'Hentai' || genres._id === 'Ecchi' || genres._id === 'Boys Love' || genres._id === 'Girls Love' || genres._id === 'Erotica'){
           console.log("NSFW")
       } else {
@@ -36,19 +35,5 @@ function App() {
       <Footer/>
     </div>
   )
-}
-
-function filterGenres(genres) {
-  if(genres) {
-    for(let i = 0; i < genres.length; i++) {
-      if (genres[i] === 'Hentai' || genres[i] === 'Ecchi' || genres[i] === 'Boys Love' || genres[i] === 'Girls Love' || genres[i] === 'Erotica') {
-        const genre = genres.splice(i, 1);
-        console.log(genre)
-        return genre;
-      }
-    }
-  } else {
-    return genres;
-  }
 }
 export default App
