18098f826fd2e3801bd1f54539c55ba34a9acd1f
faseian <nab4nkert@outlook.com>
2024-07-23 14:41:20 -0700

    Working on search bar

--- ReactWorkshop/src/components/Header.jsx
+++ ReactWorkshop/src/components/Header.jsx
@@ -5,6 +5,10 @@ function Header() {
     return (
         <div className="header-container">
             <h1 className="title">Anime Lister</h1>
+            <form action="">
+                <input type="text" value="Search..." className="search-bar" id="search-bar"/>
+                <button></button>
+            </form>
         </div>
     )
 }
