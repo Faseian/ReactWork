604d99e5324c10c4699f3bf6f52e3f743331925c
faseian <nab4nkert@outlook.com>
2024-07-23 22:05:20 -0700

    Style stuff

--- ReactWorkshop/src/components/Header.jsx
+++ ReactWorkshop/src/components/Header.jsx
@@ -5,9 +5,9 @@ function Header() {
     return (
         <div className="header-container">
             <h1 className="title">Anime Lister</h1>
-            <form action="">
+            <form action="" className="form-container">
                 <input type="text" value="Search..." className="search-bar" id="search-bar"/>
-                <button></button>
+                <button>Search</button>
             </form>
         </div>
     )
