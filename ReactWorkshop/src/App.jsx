cc9b346966dde43e0f6804c9c71e625f3be30e6b
faseian <nab4nkert@outlook.com>
2024-08-06 22:59:21 -0400

    Fixed css

--- ReactWorkshop/src/App.jsx
+++ ReactWorkshop/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/home";
+import Home from "./components/Home";
 function App() {
     return (
         <BrowserRouter>
--- ReactWorkshop/src/components/Header.jsx
+++ ReactWorkshop/src/components/Header.jsx
@@ -5,8 +5,9 @@ function Header() {
     return (
         <div className="header-container">
             <h1 className="title">Anime Lister</h1>
-            <div className = 'spaceBetweenDiv'></div>
-            <input type='text' placeholder="Search..." className="search-bar" id="search-bar"/>
+            <div className = 'search-box'>
+                <input type='text' placeholder="Search..." className="search-bar" id="search-bar"/>
+            </div>
         </div>
     )
 }
